2ef4e8aad0c9c7ce47bc181ee8f3643793310c70
Burence1 <burensomondi@gmail.com>
2021-03-11 09:19:02 +0300

    add javascript

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
 $(document).ready(function () {
-  $("#").click(function () {
-    $("#").show();
-    $("#").hide();
-  })
-});
+  $("#donate-form-1").submit(function (event) {
+    event.preventDefault();
+    $("#btn-submit").click(function(){
+      var fname = $("#fname").val();
+      var lname = $("#lname").val();
+      var amount = $("input:radio[type = radio]:checked").val();
+      var other = $("#other").val();
+      var phone = $("#phone").val();
+
+      $("#showFname").text(fname);
+      $("#showLname").text(lname);
+      $("#showAmount").text(amount);
+      $("#showOther").text(other);
+      $("#showPhone").text(phone);
+      console.log(fname);
+
+      
+    });
+    
+  })});
+  
