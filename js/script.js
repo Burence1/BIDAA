57b1db1f3d4ee0dab3e1d244441b0abf0b5dfe17
anzalmohamed <anzaldidah@gmail.com>
2021-03-11 12:42:05 +0300

    combined changes

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $("#donate-form-1").submit(function (event) {
+      event.preventDefault();
+      $("#btn-submit").click(function(){
+        var fname = $("#fname").val();
+        var lname = $("#lname").val();
+        var amount = $("input:radio[type = radio]:checked").val();
+        var other = $("#other").val();
+        var phone = $("#phone").val();
+  
+        $("#showFname").text(fname);
+        $("#showLname").text(lname);
+        $("#showAmount").text(amount);
+        $("#showOther").text(other);
+        $("#showPhone").text(phone);
+        console.log(fname);
+  
+        
+      });
+      
+    })});
+    
+  
